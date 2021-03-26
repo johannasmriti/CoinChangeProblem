f4fbf1c3fe123db5c7498256d3294597eaf4c2fb
Johanna Smriti <64794456+johannasmriti@users.noreply.github.com>
2021-03-26 10:02:44 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,27 +3,25 @@ using namespace std;
 
 class Solution {
 public:
-	int coinChange(int len,int coins[], int amount) {
-		int dp[amount+1];
-		dp[0]=0;
-		for(int i=1;i<=amount;++i)
-			dp[i] = INT_MAX;
-		
-		//int len = coins.size();
-
-		for (int i = 1; i <= amount; ++i)
-		{
-			for (int j = 0; j < len; ++j)
-			{
-				if(coins[j] <= i){
-					int rest = dp[i-coins[j]];
-					if(rest != INT_MAX && rest+1<dp[i]){
-						dp[i] = rest+1;
-					}
-				}
-			}
-		}
-		return dp[amount]==INT_MAX ? -1 : dp[amount];
+	int coinChange(int n,int coins[], int amount) {
+    
+        int dp[n+1][amount+1];
+        
+        for(int i=0;i<=n;++i)
+        {
+            for(int j=0;j<=amount;++j)
+            {
+                if(j==0)
+                    dp[i][j] = 0;
+                else if(i==0)
+                    dp[i][j] = 1e5;
+                else if(coins[i-1]>j)
+                    dp[i][j] = dp[i-1][j];
+                else
+                    dp[i][j] = min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
+            }
+        }
+        return dp[n][amount]>1e4 ? -1:dp[n][amount];
 	}
 };
 
